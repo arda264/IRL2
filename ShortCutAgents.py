28e9a21a77c9104e39c962a7411575a107d89d18
A. Çavușoğlu <a.cavusoglu@umail.leidenuniv.nl>
2026-04-10 13:05:30 +0200

    only nstep SARSA left

--- ShortCutAgents.py
+++ ShortCutAgents.py
@@ -1,6 +1,4 @@
 import numpy as np
-#import matplotlib as plt
-import scipy as sci
 import ShortCutEnvironment as SCE
 
 class QLearningAgent(object):
@@ -62,6 +60,7 @@ class QLearningAgent(object):
             episode_returns.append(cumulative_reward)
 
         return episode_returns
+
 
 class SARSAAgent(object):
     def __init__(self, n_actions, n_states, epsilon=0.1, alpha=0.1, gamma=1.0):
@@ -133,7 +132,6 @@ class SARSAAgent(object):
 
         return episode_returns
 
-
 class ExpectedSARSAAgent(object):
 
     def __init__(self, n_actions, n_states, epsilon=0.1, alpha=0.1, gamma=1.0):
@@ -143,23 +141,78 @@ class ExpectedSARSAAgent(object):
         self.alpha = alpha
         self.gamma = gamma
         # TO DO: Initialize variables if necessary
+
+        self.Q = np.zeros((n_states, n_actions)) #Initialize the action values Q(s,a) to 0.
         
     def select_action(self, state):
         # TO DO: Implement policy
+
+        #Implement an ϵ-greedy policy for selecting an action.
         action = None
+
+        best_action = np.argmax(self.Q[state, :])
+
+        #from our previous assignment
+        probabilities = np.multiply(np.ones(self.n_actions), np.divide(self.epsilon, (self.n_actions -1))) #making every choice probability the same
+        probabilities[best_action] = (1 - self.epsilon) #assigning the naive agents value to the probabilities, sums up to 1
+
+        action = np.random.choice(self.n_actions, p = probabilities)
+
         return action
         
-    def update(self, state, action, reward, done): # Augment arguments if necessary
+    def update(self, state, action, reward, done, next_state): # Augment arguments if necessary
         # TO DO: Implement Expected SARSA update
-        pass
+
+        #expected sarsa update Q(St, At) = Q(St, At) + alpha * [Rt+1 + gamma * sum(pi(a|St+1) * Q(St+1, a)) - Q(St, At)]
+        predict = self.Q[state, action]
+
+        if not done:
+            best_next_action = np.argmax(self.Q[next_state, :])
+            
+            expected_q = 0
+
+            for a in range(self.n_actions):
+                if a == best_next_action:
+                    expected_q += (1 - self.epsilon) * self.Q[next_state, a]
+                
+                else:
+                    expected_q += (self.epsilon / (self.n_actions - 1)) * self.Q[next_state, a]
+            
+            target = reward + self.gamma * expected_q  
+        
+        else:
+            target = reward
+        
+        self.Q[state, action] += self.alpha * (target - predict)
 
     def train(self, n_episodes):
         # TO DO: Implement the agent loop that trains for n_episodes. 
         # Return a vector with the the cumulative reward (=return) per episode
+        
         episode_returns = []
-        return episode_returns    
+        env = SCE.ShortcutEnvironment()
+        
+        for _ in range(n_episodes):
+            env.reset()
+            state = env.y * env.c + env.x
+            cumulative_reward = 0
 
+            while not env.isdone:
+                action = self.select_action(state)
+                
+                reward = env.step(action)
+                next_state = env.y * env.c + env.x
+                done = env.isdone
+                
+                self.update(state, action, reward, done, next_state)
+                
+                state = next_state
+                cumulative_reward += reward
 
+            episode_returns.append(cumulative_reward)
+            
+        return episode_returns   
+    
 class nStepSARSAAgent(object):
 
     def __init__(self, n_actions, n_states, n, epsilon=0.1, alpha=0.1, gamma=1.0):
--- ShortCutExperiment.py
+++ ShortCutExperiment.py
@@ -4,9 +4,33 @@ import ShortCutEnvironment as SCE
 import ShortCutAgents as SCA
 import matplotlib as plt
 
+#For Q and SAARS we do it in env below.
 env = SCE.ShortcutEnvironment()
 
+#Check Q Agent
+
 agent = SCA.QLearningAgent(env.action_size(), env.state_size())
+agent.train(10000)
+
+#print(agent.Q, "done")
+#env.render()
+env.render_greedy(agent.Q)
+
+#Check SARSA
+
+agent = SCA.SARSAAgent(env.action_size(), env.state_size())
 agent.train(1000)
 
-print(agent.Q)
+#print(agent.Q, "done")
+#env.render()
+env.render_greedy(agent.Q)
+
+#Check ExpectedSARSA
+
+agent = SCA.ExpectedSARSAAgent(env.action_size(), env.state_size())
+agent.train(1000)
+
+#print(agent.Q, "done")
+#env.render()
+env.render_greedy(agent.Q)
+
